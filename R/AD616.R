1061952c15aae4345b53828e2d899ad85778484b
ravi <ravid@bu.edu>
2020-12-16 10:50:04 -0500

    Added a member variable for DecisionPath

--- R/AD616.R
+++ R/AD616.R
@@ -10,7 +10,9 @@ Tree <- R6Class("Decision Trees", list(
   GetNodeLabel = NULL,
   GetNodeShape = NULL,
   node_list = c(),
-  emv_val = 0,
+  EMV = 0,
+  max = 0,
+  DecisionPath = c(),
   initialize = function(name,type='decision') {
     stopifnot(is.character(name), length(name) == 1)
     self$name <- name
@@ -46,7 +48,7 @@ Tree <- R6Class("Decision Trees", list(
     stopifnot(is.character(pNode), length(pNode) == 1)
     #stopifnot(is.numeric(prob), length(prob) == 1)
     stopifnot(name %ni% colnames(df))
-    print(self$node_list[pNode])
+#    print(self$node_list[pNode])
     self$node_list[name] <- list(self$node_list[pNode][[1]]$AddChild(name, type = 'decision',route = route,p=prob))
   },
   addChance = function(pNode,name,cost,route='',prob=1){
@@ -56,7 +58,7 @@ Tree <- R6Class("Decision Trees", list(
     stopifnot(name %ni% colnames(df))
     self$node_list[name] <- list(self$node_list[pNode][[1]]$AddChild(name, type = 'chance',route = route,cost=cost,p=prob))
   },
-  addTerminal = function(pNode,name,prob=1.0,payoff){
+  addTerminal = function(pNode,name,route,prob=1.0,payoff){
     stopifnot(is.character(name), length(name) == 1)
     stopifnot(is.character(pNode), length(pNode) == 1)
     stopifnot(is.numeric(payoff), length(payoff) == 1)
@@ -65,6 +67,7 @@ Tree <- R6Class("Decision Trees", list(
     self$node_list[name] <- list(self$node_list[pNode][[1]]$AddChild(name, type = 'terminal',payoff=payoff,p=prob))
   },
   update_payoff = function(){
+    dpath <- c()
     payoff <- function(node) {
       if (node$type == 'chance') node$payoff = node$cost + sum(sapply(node$children, function(child) child$payoff * child$p))
       else if (node$type == 'decision') {
@@ -72,7 +75,20 @@ Tree <- R6Class("Decision Trees", list(
       }
     }
     self$root[[1]]$Do(payoff, traversal = "post-order", filterFun = isNotLeaf)
-    emv_val = self$root[[1]]$payoff
+    self$EMV = self$root[[1]]$payoff
+    decision <- function(node){
+
+      if (node$payoff > self$max)
+      {
+        self$max = node$payoff
+        self$DecisionPath <- append(self$DecisionPath,node$name)
+
+      }
+
+    }
+
+    self$root[[1]]$Do(decision, traversal = "level",filterFun=isNotLeaf)
+
     self$GetNodeLabel= function(node) switch(node$type, terminal=node$payoff, chance=paste(node$name,"(EV =",node$payoff,")"),decision=paste(node$name,"(EMV =",node$payoff,")"),branch=node$name)
     SetNodeStyle(self$root[[1]], fontname = 'helvetica', fontsize=18,shape = self$GetNodeShape, label = self$GetNodeLabel)
   },
